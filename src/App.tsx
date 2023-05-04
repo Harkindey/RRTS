1e3f42dabb9e9e3296122ea0867ba7ff2123ee20
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-05-04 21:39:48 +0100

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './App.css';
 
 type Props = {color:string}
 
 const App:FC<Props> = ({color}) =>{
+  const [state, setState] = useState({counter: 0})
+
+  const onIncrement =():void=> {
+    setState(v=>({counter: v.counter+1}))
+  }
+  const onDecrement =():void=> {
+    setState(v=>({counter: v.counter-1}))
+  }
   return (
     <div className="App">
-       {color}
+       <button onClick={onIncrement}>Increment</button>
+       <button onClick={onDecrement}>Decrement</button>
+       {state.counter}
     </div>
   );
 }
