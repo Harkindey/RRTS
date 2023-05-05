a871bea9825dbddae494aadba6df36c12f9b2dc8
Emmanuel Akinde <emmanuelreactdev@gmail.com>
2023-05-05 23:59:39 +0100

    type definition files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,22 +2,27 @@ import React, { FC, useEffect, useState } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { StoreState } from '../reducers';
-import { Todo, fetchTodos } from '../actions';
+import { Todo, deleteTodo, fetchTodos } from '../actions';
 
 type Props = {
   todos: Todo[];
-  fetchTodos: () => void
+  fetchTodos: () => void;
+  deleteTodo: typeof deleteTodo;
 }
 
-const App:FC<Props> = ({todos, fetchTodos}) =>{
+const App:FC<Props> = ({todos, fetchTodos, deleteTodo}) =>{
 
   const click = (): void=>{
     fetchTodos()
   }
 
+const onTodoClick = (id: number)=> {
+  deleteTodo(id)
+}
+
   const renderList = ():JSX.Element[]=>{
     return todos.map((todo)=>(
-    <div key={todo.id}>
+    <div key={todo.id} onClick={()=> onTodoClick(todo.id)}>
      {todo.id}. {todo.title}
     </div>
     ))
@@ -36,5 +41,6 @@ const mapStateToProps = ({todos}: StoreState) => {
 }
 
 export default connect(mapStateToProps, {
-  fetchTodos
+  fetchTodos,
+  deleteTodo
 })(App);
